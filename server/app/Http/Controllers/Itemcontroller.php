2c0adc0e84c0ad5b4621e5103849156d245533f8
KEISUKE-KITAGAWA <casekk1021@yahoo.co.jp>
2020-07-25 16:21:03 +0900

    一覧ページ完了

--- server/app/Http/Controllers/Itemcontroller.php
+++ server/app/Http/Controllers/Itemcontroller.php
@@ -7,10 +7,19 @@ use Illuminate\Http\Request;
 
 class Itemcontroller extends Controller
 {
+    // indexページへ移動
+    public function index()
+    {
+        // モデル名::テーブル全件取得
+        $items = Item::all();
+        // Itemsティレクトリーの中のindexページを指定し、itemsの連想配列を代入
+        return view('Items.index', ['items' => $items]);
+    }
+    
     public function show($id)
     {
         $item = Item::find($id);
         return view('Items.show', ['item' => $item]);
-}
+    }
 
 }
